9f4cb3f05762717a6acd8696281d63879727031c
Mason Fox <masonfox22@gmail.com>
2026-02-04 08:07:00 -0500

    make script DRY and fix tome path

--- scripts/sync-tome.sh
+++ scripts/sync-tome.sh
@@ -46,6 +46,19 @@ else
   info "Mounted foxnas-v2.local:/volume3/nvme to $NVME_MOUNT"
 fi
 
+# Sync functions
+sync_calibre() {
+  info "Syncing Calibre..."
+  rsync -av --delete --exclude='.caltrash/' "$NVME_MOUNT/ebooks/" /home/mason/Calibre/prod-copy/
+  info "Calibre sync complete!"
+}
+
+sync_tome() {
+  info "Syncing Tome..."
+  rsync -av --exclude='backups/' --exclude='*.zip' "$DOCKER_MOUNT/tome/nightly/" /home/mason/git/tome/data/
+  info "Tome sync complete!"
+}
+
 # Prompt user for sync choice
 echo ""
 echo "What would you like to sync?"
@@ -57,24 +70,15 @@ read -p "Enter your choice (1-3): " choice
 
 case $choice in
 1)
-  info "Syncing Calibre..."
-  rsync -av --delete "$NVME_MOUNT/ebooks/" /home/mason/Calibre/prod-copy/
-  info "Calibre sync complete!"
+  sync_calibre
   ;;
 2)
-  info "Syncing Tome..."
-  rsync -av "$DOCKER_MOUNT/tome/nightly/data/" /home/mason/git/tome/data/
-  info "Tome sync complete!"
+  sync_tome
   ;;
 3)
   info "Syncing both Calibre and Tome..."
-
-  info "Syncing Calibre..."
-  rsync -av --delete "$NVME_MOUNT/ebooks/" /home/mason/Calibre/prod-copy/
-
-  info "Syncing Tome..."
-  rsync -av "$DOCKER_MOUNT/tome/nightly/data/" /home/mason/git/tome/data/
-
+  sync_calibre
+  sync_tome
   info "All syncs complete!"
   ;;
 *)
